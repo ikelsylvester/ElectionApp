f99de12e7b472f74a2a13d4c1b1dd4711ff652c4
Ikel Sylvester jr <167588167+088020634@users.noreply.github.com>
2024-07-17 15:27:48 +0000

    I added database

--- server.js
+++ server.js
@@ -12,6 +12,31 @@ app.use(bodyparser.urlencoded({extended:true}))
 app.use(bodyparser.json())
 let users= []
 
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./election.db')
+
+db.serialize(() => {
+   db.run('CREATE TABLE IF NOT EXISTS auth( id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(50) NOT NULL, password VARCHAR(50) NOT NULL, user_id INT)');
+
+   db.run('CREATE TABLE IF NOT EXISTS roles( id INT AUTO_INCREMENT PRIMARY KEY, role VARCHAR(50) NOT NULL)');
+
+   db.run('CREATE TABLE IF NOT EXISTS users( id INT AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(50) NOT NULL,   middle_name VARCHAR(50) NULL,last_name VARCHAR(50) NOT NULL, DOB DATE NOT NULL)');
+
+   db.run('CREATE TABLE IF NOT EXISTS parties( id INT AUTO_INCREMENT PRIMARY KEY, party VARCHAR(50) NOT NULL,logo blob)');
+
+   db.run('CREATE TABLE IF NOT EXISTS positions( id INT AUTO_INCREMENT PRIMARY KEY, position VARCHAR(50) NOT NULL,logo blob)');
+
+   db.run('CREATE TABLE IF NOT EXISTS candidates( id INT AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(50) NOT NULL,   middle_name VARCHAR(50) NULL,last_name VARCHAR(50) NOT NULL, position_id INT NOT NULL,party_id INT NOT NULL, photo blob )');
+
+   db.run('CREATE TABLE IF NOT EXISTS votes( id INT AUTO_INCREMENT PRIMARY KEY, candidate_id INT NOT NULL, vote INT NOT NULL )');
+
+  db.each('SELECT * FROM auth', (err, row) => {
+    console.log(row)
+  }) 
+})
+
+db.close()
+
 app.get("/dashboard", (req,res)=>{
     res.render("dashboard.ejs")
 })
